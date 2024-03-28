56e5ed6fd8000c973cbb92d6119f8a13f2c0ab37
barpacay <barpacay@uncc.edu>
2024-03-28 13:07:09 -0400

    Co-authored-by: malazzeh1 <malazzeh1@users.noreply.github.com>

--- weather-react-main/src/App.js
+++ weather-react-main/src/App.js
@@ -20,6 +20,7 @@ function App() {
 
   return (
     <div className="app">
+      <img src="/weather-react-main/src/assets/Logo_Weather.png" alt="Logo" class="logo"></img>
       <div className="search">
         <input
           value={location}
@@ -29,10 +30,39 @@ function App() {
           type="text" />
       </div>
       <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>{data.name}</p>
+        <div className="weather">
+          {data.weather ? <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt="icon" /> : null}
+          {new Date().toDateString()}
+          <h1>{data?.name}</h1>
+        </div>
+        <div className="dashboard">
+        <div className="temp_min">
+            {data.main ? <h1>{data.main.temp_min.toFixed()}°F</h1> : null}
+            <h2>Low</h2>
           </div>
+          <div className="temp_max">
+            {data.main ? <h1>{data.main.temp_max.toFixed()}°F</h1> : null}
+            <h2>High</h2>
+          </div>
+          <div className="humidity">
+            {data.main ? <h1>{data.main.humidity.toFixed()}%</h1> : null}
+            <h2>Humidity</h2>
+          </div>
+          <div className="wind">
+            {data.main ? <h1>{data.wind.speed.toFixed()} MPH </h1> : null}
+            <h2>Wind</h2>
+          </div>
+          <div className="feels_like">
+            {data.main ? <h1>{data.main.feels_like.toFixed()}°F</h1> : null}
+            <h2>Feels Like</h2>
+          </div>
+          <div className="pressure">
+            {data.main ? <h1>{data.main.pressure} </h1> : null}
+            <h2>Pressure</h2>
+          </div>
+        </div>
+        <div className="top">
+
           <div className="temp">
             {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
